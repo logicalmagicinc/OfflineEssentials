5d4b2ce734aeec0ad9584b5d7fe804cded8371f3
Justin Allen <all12jus@gmail.com>
2024-04-10 09:55:55 -0400

    Update NetworkService.swift

--- Sources/OfflineEssentials/Services/NetworkService.swift
+++ Sources/OfflineEssentials/Services/NetworkService.swift
@@ -61,6 +61,58 @@ public class NetworkService {
         }
     }
     
+    public func POST<R: Codable>(url endpoint: String, token: String?) async -> Result<R, Error> {
+        guard let url = URL(string: "\(baseUrl)/\(endpoint)") else {
+            // TODO: swap this to the Error case.
+            return .failure(NetworkError.invalidURL)
+        }
+        do {
+            var request = URLRequest(url: url)
+            //            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            request.httpMethod = "POST"
+            if let token {
+                request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+            }
+            
+//            if let content {
+//                let data = try JSONEncoder.network.encode(content)
+//                //                print("POSTING - \(data)")
+//                request.httpBody = data
+//                //                let str = String(decoding: data, as: UTF8.self)
+//                //                print(str)
+//            }
+            //            print(content)
+            
+            
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            //            let str = String(decoding: data, as: UTF8.self)
+            //            print(str)
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200..<300:
+                    let result = try JSONDecoder.network.decode(R.self, from: data)
+                    return .success(result)
+                case 401:
+                    return .failure(NetworkError.unauthenticated)
+                default:
+                    // Here, you can handle different error cases based on status codes.
+                    // You might want to decode the error response from the server and create a custom error.
+                    // For now, I'll return a generic HTTPError with the status code.
+                    return .failure(HTTPError(statusCode: httpResponse.statusCode))
+                }
+            }
+            
+            return .failure(NetworkError.unknown)
+        } catch {
+            print(error)
+            return .failure(error)
+        }
+    }
+    
     public func POST<Q: Codable, R: Codable>(url endpoint: String, token: String?, content: Q?) async -> Result<R, Error> {
         guard let url = URL(string: "\(baseUrl)/\(endpoint)") else {
             // TODO: swap this to the Error case.
@@ -136,6 +188,47 @@ public class NetworkService {
             
 //            let str = String(decoding: data, as: UTF8.self)
 //            print(str)
+            
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200..<300:
+                    let result = try JSONDecoder.network.decode(R.self, from: data)
+                    return .success(result)
+                case 401:
+                    return .failure(NetworkError.unauthenticated)
+                default:
+                    // Here, you can handle different error cases based on status codes.
+                    // You might want to decode the error response from the server and create a custom error.
+                    // For now, I'll return a generic HTTPError with the status code.
+                    return .failure(HTTPError(statusCode: httpResponse.statusCode))
+                }
+            }
+            
+            return .failure(NetworkError.unknown)
+        } catch {
+            print(error)
+            return .failure(error)
+        }
+    }
+    
+    public func DELETE<R: Codable>(url endpoint: String, token: String?) async -> Result<R, Error> {
+        guard let url = URL(string: "\(baseUrl)/\(endpoint)") else {
+            // TODO: swap this to the Error case.
+            return .failure(NetworkError.invalidURL)
+        }
+        do {
+            var request = URLRequest(url: url)
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            request.httpMethod = "DELETE"
+            if let token {
+                request.setValue( "Bearer \(token)", forHTTPHeaderField: "Authorization")
+            }
+            
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            //            let str = String(decoding: data, as: UTF8.self)
+            //            print(str)
             
             if let httpResponse = response as? HTTPURLResponse {
                 switch httpResponse.statusCode {
